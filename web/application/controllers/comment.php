57799ff7abadce89e02674072c67dacc82ff252b
Arun Krishnakumar <arunxls@gmail.com>
2014-10-25 14:28:00 -0500

    Changed controller +

--- web/application/controllers/comment.php
+++ web/application/controllers/comment.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Comment extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -17,9 +17,6 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index() {
-        $this->load->model('bro_table');
-	}
 
 	public function new_comment() {
 		$data = $this->input->post();
